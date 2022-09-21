99e1070f2d7426f3958a4a022e78d0a91c6f744c
Adam Škarda <skardada@fel.cvut.cz>
2022-09-21 12:46:09 +0200

    Added utility functions

---
+++ utils/mapping.py
@@ -0,0 +1,10 @@
+
+def binary(data : list, bounds : tuple):
+    return 0
+
+def main():
+    print(binary([1,1,1,1], (0,1)))
+
+if __name__ == "__main__":
+    main()
+
---
+++ utils/perturbation.py
@@ -0,0 +1,12 @@
+import random
+import copy
+
+def binary(data : list, probability):
+    perturbed = []
+    for i in data:
+        if random.random() <= probability:
+            perturbed.append(0 if 1 else 1)
+        else:
+            perturbed.append(copy.deepcopy(i))
+
+    return perturbed
